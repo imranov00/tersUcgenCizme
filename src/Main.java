54241eb17fbbf452e733d2a4339e69f41e7e28e8
İmranov <semtexpert@gmail.com>
2023-12-27 16:23:59 +0300

    version 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Basamak sayısnı giriniz: ");
+        int number = input.nextInt();
+
+        for (int i=number-3; i>=0; i--)
+        {
+            for (int j=0; j<(number - i - 1); j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
